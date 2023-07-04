e2c1ebab90fbbe001e82cfbace763066749f76d4
k-taro-reccos <tarou3806@gmail.com>
2023-07-04 17:47:31 +0900

    ♻️

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import { Layout } from "/components/Layout";
 export default function Home() {
   return (
     <Layout title="Home">
-      <p className="text-4xl">こんにちは</p>
+      <p className="text-4xl">こんにちは!!</p>
     </Layout>
   );
 }
